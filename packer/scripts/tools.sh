b019c4067d94d221dd45c707a0a90a20cc845f25
Peter Parente <parente@cs.unc.edu>
2013-11-30 00:46:45 -0500

    tottbox 2013-11-29 build
    Simplify to planned topics. Rely on package managers to install other
    libs as needed throughout.

--- packer/scripts/tools.sh
+++ packer/scripts/tools.sh
@@ -16,55 +16,37 @@ EOP
 sudo apt-get -y install git
 ln -s /vagrant/.gitconfig ~/.gitconfig
 
-# ipython, virtualenv + wrapper, pip
+# pip and virtualenv
 sudo apt-get -y install python-dev python-pip
 sudo pip install virtualenv
-sudo pip install virtualenvwrapper
 sudo pip install -U distribute
+
+# ipython, pandas, matplotlib, etc.
+sudo apt-get -y install freetype* libpng-dev g++
+sudo pip install pyzmq
+sudo pip install jinja2
 sudo pip install ipython
+sudo pip install numpy
+sudo pip install pandas
+sudo pip install matplotlib
+sudo pip install scipy
+sudo pip install scikit-learn
+
+# behave
+sudo pip install behave
 
 # nodejs, npm, nvm
 wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
 . /home/vagrant/.profile
 echo '. /home/vagrant/.profile' >> /home/vagrant/.bash_profile
-nvm install v0.10.17
-nvm alias default v0.10.17
+nvm install v0.10.22
+nvm alias default v0.10.22
 
-# yeoman, grunt, bower, generators for yeoman
-# need fontconfig else phantomjs goes kaput
-sudo apt-get -y install fontconfig
-npm install -g mocha phantomjs yo grunt-cli bower
+# express, mocha, and bower
+npm install -g bower mocha
 npm cache clean
 
-# mongodb
-sudo apt-get -y install mongodb
-
-# sphinx, hieroglyph, groc
-sudo pip install sphinx
-sudo pip install hieroglyph
-npm install -g groc
-
-# ruby, cf
+# rvm, ruby
 \curl -L https://get.rvm.io | bash -s stable
 . /home/vagrant/.rvm/scripts/rvm
 rvm install 1.9.3
-#rvm gemset create cf
-#rvm use 1.9.3@cf --default
-gem install cf --no-ri --no-rdoc
-# need compass for some yeoman grunt tasks
-gem install compass --no-ri --no-rdoc
-
-# python fabric, nose, behave
-sudo pip install fabric
-sudo pip install nose
-sudo pip install behave
-
-# mocha
-npm install -g mocha
-
-# pandas, matplotlib, notebook
-sudo apt-get -y install freetype* libpng-dev
-# fails if all three on the same line ...
-sudo pip install numpy
-sudo pip install pandas
-sudo pip install matplotlib
