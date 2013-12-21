da564bdc0087953f776b4c0618951c6d8bb3fa2c
Peter Parente <parente@cs.unc.edu>
2013-12-20 20:00:32 -0500

    Fixed #5, use ogg/ogv when needed

--- doc/slides/casts/js/narrator.js
+++ doc/slides/casts/js/narrator.js
@@ -1,4 +1,5 @@
 (function() {
+    var ext = '.mp3';
     var top = document.querySelector('.reveal');
     var narrator = document.createElement('div');
     narrator.className = 'narrator';
@@ -8,16 +9,26 @@
     narrator.appendChild(audio);
     top.appendChild(narrator);
 
+    // from diveintohtml5 appendix a
+    if(!!(audio.canPlayType && audio.canPlayType('audio/mpeg;').replace(/no/, ''))) {
+        ext = '.mp3';
+    } else if(!!(audio.canPlayType && audio.canPlayType('audio/ogg; codecs="vorbis"').replace(/no/, ''))) {
+        ext = '.ogg';
+    } else {
+        console.error('browser does not support mp3 or ogg');
+    }
+    console.log('using', ext, 'audio');
+
     var set_narration = function(slide) {
         audio.pause();
         if(slide.dataset && slide.dataset.narration) {
-            var url = Reveal.getConfig().narrationRoot + slide.dataset.narration;
+            var url = Reveal.getConfig().narrationRoot + slide.dataset.narration + ext;
             narrator.classList.remove('disabled');
             narrator.classList.remove('hidden');
             audio.removeAttribute('disabled');
             audio.setAttribute('src', url);
         } else {
-            audio.setAttribute('disabled');
+            audio.setAttribute('disabled', true);
             if(slide.querySelector('video')) {
                 narrator.classList.add('hidden');
             } else {
@@ -27,11 +38,9 @@
     };
 
     var set_jsbin = function(slide) {
-        console.log(slide);
         if(slide.dataset && slide.dataset.jsbin) {
             var path = slide.dataset.jsbin;
             var iframe = slide.querySelector('.jsbin-embed');
-            console.log('src', iframe.src);
             if(!iframe.src) {
                 iframe.src = 'http://jsbin.com'+path;
             }
