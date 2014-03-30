062f5962310091b2969ebbb88d01986dc2d8c5d4
Peter Parente <parente@cs.unc.edu>
2014-03-30 19:59:40 -0400

    Cleanup media converter

--- media/convert.sh
+++ media/convert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v "$PWD":"$PWD" -a stdout -a stderr -w "$PWD" ffmpeg ./convert.py $@
+docker run -v "$PWD":"$PWD" -a stdout -a stderr -w "$PWD" parente/tott-media ./convert.py $@
